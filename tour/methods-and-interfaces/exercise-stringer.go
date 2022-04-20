5c484d52d07ed59efb75d3fff2524a7ace8ef926
kopever <kop.still@gmail.com>
2022-04-20 18:21:02 +0800

    stringer exercise

--- tour/methods-and-interfaces/exercise-stringer.go
+++ tour/methods-and-interfaces/exercise-stringer.go
@@ -8,8 +8,11 @@ import (
 
 type IPAddr [4]byte
 
+//func (ip IPAddr) String() string {
+//	return fmt.Sprintf("%d.%d.%d.%d", ip[0], ip[1], ip[2], ip[3])
+//}
+
 func (ip IPAddr) String() string {
-	//return fmt.Sprintf("%d.%d.%d.%d", ip[0], ip[1], ip[2], ip[3])
 
 	result := make([]string, len(ip))
 	for i, v := range ip {
@@ -18,6 +21,19 @@ func (ip IPAddr) String() string {
 
 	return strings.Join(result, ".")
 }
+
+//func (ip IPAddr) String() string {
+//	var s string
+//	for i := range ip {
+//		if i == 0 {
+//			s += fmt.Sprint(int(ip[i]))
+//		} else {
+//			s += "." + fmt.Sprint(int(ip[i]))
+//		}
+//	}
+//
+//	return s
+//}
 
 func main() {
 	hosts := map[string]IPAddr{
